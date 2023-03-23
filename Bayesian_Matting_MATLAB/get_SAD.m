1094a6cde3c1f7b85e139d9cab524959546347a2
Laban <labanr@AD.MEE.TCD.IE>
2023-03-23 10:31:04 +0000

    deleting all files

--- Bayesian_Matting_MATLAB/get_SAD.m
+++
@@ -1,8 +0,0 @@
-function [sad] = get_SAD(ground_truth, alpha)
-img1 = im2double(ground_truth(:,:,1));
-img2 = alpha;
-
-% Compute the SAD
-sad = sum(abs(img1 - img2), 'all');
-
-% sad = sum(sum(imabsdiff(img1, img2)))
