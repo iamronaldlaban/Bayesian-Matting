15e9d332502bcd1e590ce6b86e6996197bb326ad
Laban <labanr@AD.MEE.TCD.IE>
2023-04-05 19:16:15 +0100

    Python: Final Main

--- Bayesian_Matting_Python/main_2.py
+++ Bayesian_Matting_Python/main_2.py
@@ -2,17 +2,15 @@ import numpy as np
 from PIL import Image, ImageOps
 import matplotlib.pyplot as plt
 import cv2
-import os
 from matting_functions import compositing
 from Bayesian_matte_OB import Bayesian_Matte
-from Bayesian_matte_KNN import Bayesian_Matte_KNN
 from quality_metrics import quality_metrics
 from timeit import default_timer as timer
 import datetime
-import tkinter as tk
-from tkinter import filedialog
 import unittest
+from orchard_bouman_clust import calc_mean, calc_total_weight
 from matting_functions import matlab_style_gauss2d
+
 
 
 # root = tk.Tk()
@@ -21,87 +19,54 @@ from matting_functions import matlab_style_gauss2d
 # image = filedialog.askopenfilename()
 # image_trimap = filedialog.askopenfilename()
 
-#image = np.array(Image.open('High_Resolution/input_training_highres/GT04.png'))
-#image_trimap = np.array(ImageOps.grayscale(Image.open('High_Resolution/trimap_training_highres/Trimap1/GT04.png')))
-image = np.array(Image.open('input_training_lowres\GT01.png'))
-image_trimap = np.array(ImageOps.grayscale(Image.open('trimap_training_lowres\Trimap1\GT01.png')))
-
+# Read the image, trimap and ground truth
+filename = 'GT10'
+image = np.array(Image.open(f'input_training_lowres\{filename}.png'))
+image_trimap = np.array(ImageOps.grayscale(Image.open(f'trimap_training_lowres\Trimap2\{filename}.png')))
+ground_truth = np.array(ImageOps.grayscale(Image.open(f'gt_training_lowres/{filename}.png')))
 # Window Size
 N = 105
 
-start = timer()
+start = timer() # Start the timer
 
+# Run the Bayesian Matting algorithm
 alpha_OB= Bayesian_Matte(image, image_trimap, N)
-end = timer()
-# alpha_OB[alpha_OB < 0.5] = 0
-# alpha_OB[alpha_OB >= 0.5] = 1
-alpha_OB = alpha_OB*255
+end = timer() # End the timer
+alpha_OB = alpha_OB*255 # Convert to 8-bit image
 
-# write_path = 'C:/Users/labanr/Desktop/Matting/Bayesian-Matting/Bayesian_Matting_Python/alpha.png'
-# cv2.imwrite(write_path, alpha_OB)
-
-# image_alpha = np.array(ImageOps.grayscale(Image.open('High_Resolution/gt_training_highres/GT04.png')))
-ground_truth = np.array(ImageOps.grayscale(Image.open('gt_training_lowres/GT01.png')))
+# Calculate the quality metrics
 quality_metrics(alpha_OB, ground_truth)
 print('Time taken: ', datetime.timedelta(seconds = end - start))
-# plt.imshow(alpha_OB, cmap='gray')
-# plt.show()
+
+# Read the background image
 background = cv2.imread('C:/Users/labanr/Desktop/Matting/Bayesian-Matting/Bayesian_Matting_Python/background.jpg')
 background2 = cv2.cvtColor(background, cv2.COLOR_BGR2RGB)
 
-# comp_OB = compositing(image, alpha_OB, background2)
-# plt.imshow(comp_OB)
-# plt.show()
+# Compositing
+comp_OB = compositing(image, alpha_OB, background2)
 
+# Create a subplot of input image, trimap, alpha matte, and composite image
+fig, ax = plt.subplots(1, 4, figsize=(20,5))
+ax[0].imshow(image)
+ax[0].set_title('Input Image')
+ax[0].axis('off')
+ax[1].imshow(image_trimap, cmap='gray')
+ax[1].set_title('Trimap')
+ax[1].axis('off')
+ax[2].imshow(alpha_OB, cmap='gray')
+ax[2].set_title('Alpha Matte')
+ax[2].axis('off')
+ax[3].imshow(comp_OB)
+ax[3].set_title('Composite Image')
+ax[3].axis('off')
 
-# alpha_knn = Bayesian_Matte_KNN(image, image_trimap)
+plt.show()
 
-# image_alpha = np.array(ImageOps.grayscale(Image.open('C:/Users/labanr/Desktop/Matting/Bayesian-Matting/Bayesian_Matting_Python/gt_training_lowres/GT10.png')))
-
-# # Plot both images side by side using subplot
-# fig, ax = plt.subplots(1, 2, figsize=(8, 4))
-
-# # Display Orchard-Bouman image on the left
-# alpha_OB = alpha_OB*255
-# ax[0].imshow(alpha_OB, cmap='gray')
-# ax[0].set_title('Orchard-Bouman')
-
-# # Display KNN image on the right
-# alpha_KNN = alpha_knn*255
-# ax[1].imshow(alpha_KNN, cmap='gray')
-# ax[1].set_title('KNN')
-
-# # Display the plot
-# plt.show()
-
-
-# background = cv2.imread('C:/Users/labanr/Desktop/Matting/Bayesian-Matting/Bayesian_Matting_Python/background.jpg')
-# background2 = cv2.cvtColor(background, cv2.COLOR_BGR2RGB)
-
-# # Plot both composites side by side using subplot
-# # Create both composites
-# comp_OB = compositing(image, alpha_OB, background2)
-# comp_KNN = compositing(image, alpha_knn, background2)
-
-# # Plot both composites side by side using subplot
-# fig, ax = plt.subplots(1, 2, figsize=(8, 4))
-
-# # Display Orchard-Bouman composite on the left
-# ax[0].imshow(comp_OB)
-# ax[0].set_title('Orchard-Bouman')
-
-# # Display KNN composite on the right
-# ax[1].imshow(comp_KNN)
-# ax[1].set_title('KNN')
-
-# # Display the plot
-# plt.show()
-from numpy.testing import assert_allclose
+# unit test
 
 class TestBayesianMatte(unittest.TestCase):
     def test_image_and_alpha_size(self):
-        print('\nTesting if the image size and alpha size are the same\n')
-        
+              
         # Load the images
         try:
             input_image = image
@@ -132,6 +97,51 @@ class TestBayesianMatte(unittest.TestCase):
         assert ground_truth_image is not None, "Ground truth image is not loaded"
         # Check if images are valid
         assert input_image.shape[:2] == trimap_image.shape[:2] == ground_truth_image.shape[:2], "Images have different sizes"
-    
+
+    def setUp(self):
+        # Initialize test data
+        self.weights = np.array([1.0, 2.0, 3.0])
+        self.data = np.array([[1.0, 2.0, 3.0],
+                              [4.0, 5.0, 6.0],
+                              [7.0, 8.0, 9.0]])
+        self.total_weight = np.sum(self.weights)
+
+    def test_calc_total_weight(self):
+        # Test calc_total_weight function
+        self.assertEqual(calc_total_weight(self.weights), 6.0)
+
+    def test_calc_mean(self):
+        # Test calc_mean function
+        expected_mean = np.array([5.0, 6.0, 7.0])
+        self.assertTrue(np.allclose(calc_mean(self.data, self.weights, self.total_weight), expected_mean))
+
+    def test_output_shape(self):
+        sigma = 0.5
+        shape = (5, 5)
+        filter = matlab_style_gauss2d(shape=shape, sigma=sigma)
+        self.assertEqual(filter.shape, shape)
+
+    def test_sum_is_one(self):
+        sigma = 1.0
+        shape = (7, 7)
+        filter = matlab_style_gauss2d(shape=shape, sigma=sigma)
+        self.assertAlmostEqual(np.sum(filter), 1.0, places=6)
+
+    def test_symmetry(self):
+        sigma = 2.0
+        shape = (11, 11)
+        filter = matlab_style_gauss2d(shape=shape, sigma=sigma)
+        flipped_filter = np.flip(filter)
+        self.assertTrue(np.allclose(filter, flipped_filter))
+
+   
 if __name__ == '__main__':
     unittest.main()
+
+
+# image_alpha = np.array(ImageOps.grayscale(Image.open('C:/Users/labanr/Desktop/Matting/Bayesian-Matting/Bayesian_Matting_Python/gt_training_lowres/GT10.png')))
+# write_path = 'C:/Users/labanr/Desktop/Matting/Bayesian-Matting/Bayesian_Matting_Python/alpha.png'
+# cv2.imwrite(write_path, alpha_OB)
+#image = np.array(Image.open('High_Resolution/input_training_highres/GT04.png'))
+#image_trimap = np.array(ImageOps.grayscale(Image.open('High_Resolution/trimap_training_highres/Trimap1/GT04.png')))
+# image_alpha = np.array(ImageOps.grayscale(Image.open('High_Resolution/gt_training_highres/GT04.png')))
